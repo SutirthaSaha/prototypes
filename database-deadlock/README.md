23f0f2d13a877e0afd356c0e11fb6b7a705f3e9a
SutirthaSaha <sutirthasaha12@gmail.com>
2024-08-17 07:17:10 +0530

    deadlock implemented with possible types

--- database-deadlock/README.md
+++ database-deadlock/README.md
@@ -13,3 +13,15 @@ Command:
 cd mysql
 docker-compose up
 ```
+
+## Lock Related Clauses:
+- `FOR UPDATE`: 
+  - This clause is used to lock the selected rows for the purpose of updating them
+  - It prevents the selected rows from being modified or locked by other transactions until the current transaction ends
+  -Other transactions trying to lock the same row with FOR UPDATE will be blocked until the lock is released
+- `FOR UPDATE NOWAIT`:
+  - When used with the above clauses (like FOR UPDATE NOWAIT), it specifies that if a requested row is locked by another transaction, the statement should not wait and instead fail immediately with an error
+  - This is useful when you don't want a transaction to be held up waiting for locks to be released
+- `FOR UPDATE SKIP LOCKED`:
+  - This clause is used to modify FOR UPDATE or FOR SHARE so that instead of waiting for locked rows or failing, it skips over them
+  - Useful in implementing job queues or similar patterns where you want to process only the rows that are not currently being processed by another transaction
--- database-deadlock/main.py
+++ database-deadlock/main.py
@@ -1,17 +1,56 @@
+import time
+from threading import Thread
+
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='secret',
-    database='employee'
-)
+
+def get_connection():
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='secret',
+        database='employee'
+    )
+    return conn
 
 
 def basic_query():
+    conn = get_connection()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM employees')
     employees = cursor.fetchall()
     for employee in employees:
         print(employee)
     cursor.close()
+    conn.close()
+
+
+def deadlock(first_id, second_id, type):
+    print("Enter the method")
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(f'SELECT * FROM employees where id = {first_id} {type}')
+    employee = cursor.fetchone()
+    print(f'Fetched first employee: {employee}')
+    print("Now let's take a break")
+    time.sleep(10)
+    print("Break complete")
+    cursor.execute(f'SELECT * FROM employees where id = {second_id} {type}')
+    employee = cursor.fetchone()
+    print(f'Fetched second employee: {employee}')
+    cursor.close()
+    conn.close()
+
+
+if __name__ == '__main__':
+    # possible types of lock
+    # FOR UPDATE, FOR UPDATE NOWAIT, FOR UPDATE SKIP LOCKED
+    type = 'FOR UPDATE SKIP LOCKED'
+    thread1 = Thread(target=deadlock, kwargs={'first_id': 1, 'second_id': 2, 'type': type})
+    thread2 = Thread(target=deadlock, kwargs={'first_id': 2, 'second_id': 1, 'type': type})
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
